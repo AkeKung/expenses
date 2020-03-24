da76d9658bd8e209b755862be47e42227b89b64e
AkeKung <46592342+AkeKung@users.noreply.github.com>
2020-03-24 22:29:23 +0700

    build

---
+++ build/precache-manifest.77b3f5b21e41421eabe8a4764b7209fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4ce6f033f0dad391536b1a7a11718d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f71ac6b7a30977744ce",
+    "url": "/static/css/main.4851e9fc.chunk.css"
+  },
+  {
+    "revision": "d24434e73aef2c6c3ae7",
+    "url": "/static/js/2.e57dfc42.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/static/js/2.e57dfc42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f71ac6b7a30977744ce",
+    "url": "/static/js/main.54e7849c.chunk.js"
+  },
+  {
+    "revision": "0d3bbb528df5eb49ddfc",
+    "url": "/static/js/runtime-main.caa9dce7.js"
+  }
+]);
